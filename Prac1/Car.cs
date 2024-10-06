343dba8ffff00cfbd8021a849a584887287144c9
Michael Chernysh <chermishan@gmail.com>
2024-10-06 15:37:11 +0300

    first commit

---
+++ Prac1/Car.cs
@@ -0,0 +1,21 @@
+﻿class Car
+{
+    private readonly ParkingLot parkingLot;
+    private readonly string carName;
+    private readonly int parkingTime;
+
+    public string CarName { get => carName; }
+    public int ParkingTime { get => parkingTime; }
+
+    public Car(ParkingLot parkingLot, string carName, int parkTime)
+    {
+        this.parkingLot = parkingLot;
+        this.carName = carName;
+        parkingTime = parkTime;
+    }
+
+    public async Task RunAsync()
+    {
+        await parkingLot.ParkCarAsync(this);
+    }
+}
---
+++ Prac1/ParkingLot.cs
@@ -0,0 +1,50 @@
+﻿class ParkingLot
+{
+    private SemaphoreSlim parkingSpots;
+    private readonly int daySpots;
+    private readonly int nightSpots;
+
+    public ParkingLot(int daySpots, int nightSpots)
+    {
+        this.daySpots = daySpots;
+        this.nightSpots = nightSpots;
+        this.parkingSpots = new SemaphoreSlim(daySpots);
+    }
+
+    public async Task ParkCarAsync(Car car)
+    {
+        if (IsParkingAllowed())
+        {
+            await parkingSpots.WaitAsync();
+            try
+            {
+                Console.WriteLine($"{car.CarName} has parked.");
+                // Симуляція часу паркування
+                await Task.Delay(car.ParkingTime);
+                Console.WriteLine($"{car.CarName} has left the parking lot.");
+            }
+            finally
+            {
+                parkingSpots.Release();
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{car.CarName} cannot park. Parking is closed.");
+        }
+    }
+
+    public void AdjustParkingSpots(bool isDayTime)
+    {
+        int availableSpots = isDayTime ? daySpots : nightSpots;
+        parkingSpots = new SemaphoreSlim(availableSpots);
+        Console.WriteLine($"Adjusted parking spots to {availableSpots}");
+    }
+
+    private bool IsParkingAllowed()
+    {
+        TimeSpan currentTime = DateTime.Now.TimeOfDay;
+        TimeSpan closingTime = new TimeSpan(19, 0, 0); // 19:00
+        return currentTime < closingTime;
+    }
+}
